a98addffbe2b8b191ec30fa7fcdd7ab6f29266ef
AdamWalus <adampw2277@gmail.com>
2023-05-23 17:07:16 +0200

    Dodaj pliki projektów.

---
+++ WpfApp3/view/appWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp3.model;
+using WpfApp3.viewmodel;
+
+namespace WpfApp3.view
+{
+    /// <summary>
+    /// Logika interakcji dla klasy appWindow.xaml
+    /// </summary>
+    public partial class appWindow : Window
+    {
+        private StringListViewModel _stringListViewModel;
+        public appWindow()
+        {
+            InitializeComponent();
+
+
+
+            var model = new StringListModel
+            {
+                Items = new ObservableCollection<string>
+                {
+                    "A",
+                    "B",
+                    "C",
+                    "D",
+                },
+
+            };
+            
+            _stringListViewModel = new StringListViewModel(model)
+            {
+                SelectedItem = "vb"
+        };
+            DataContext = _stringListViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _stringListViewModel.Items.Add(_stringListViewModel.NewItem);
+        }
+    }
+}
---
+++ WpfApp3/viewmodel/StringListViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.model;
+
+namespace WpfApp3.viewmodel
+{
+    public class StringListViewModel : INotifyPropertyChanged
+    {
+
+        public string _newItem;
+        //private ObservableCollection<string> _items;
+        private StringListModel _model;
+        private string _selectedItem;
+
+        public StringListViewModel(StringListModel model)
+        {
+            _model = model;
+        }
+       
+        public ObservableCollection<string> Items
+        {
+            get { return _model.Items; }
+            set { _model.Items= value;
+                OnPropertyChange(nameof(NewItem));
+            }
+        }
+
+
+        public string SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value;
+                OnPropertyChange(nameof(NewItem));
+            }
+        }
+        
+        public string NewItem
+        {
+            get { return _newItem; }
+            set { _newItem = value;
+                OnPropertyChange(nameof(NewItem));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+               PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
